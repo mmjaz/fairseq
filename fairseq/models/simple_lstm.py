780a3db32118eb503bb3ef1fb461194dcb2a926e
Mehdi Jaz <mmjaziryan@gmail.com>
2020-07-08 19:54:31 +0430

    Update simple_lstm.py

--- fairseq/models/simple_lstm.py
+++ fairseq/models/simple_lstm.py
@@ -6,4 +6,7 @@ from fairseq.models import FairseqEncoder
 class SimpleLSTMEncoder(FairseqEncoder):
     def __init__(self, args, dictionary, embed_dim, hidden_dim=128):
         pass
+    def forward():
+        pass
+    
 
